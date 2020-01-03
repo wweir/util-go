f96765a7d3e4ceedadcd47dd620194f79003db35
wweir <wweir@foxmail.com>
2020-01-03 11:22:45 +0800

    mysql: support grpc proto convert

--- mysql/util.go
+++ mysql/util.go
@@ -1,5 +1,7 @@
 package mysql
 
+import "fmt"
+
 // EnumToGo is mysql ENUM value to golang value
 func EnumToGo(enum interface{}) uint16 {
 	num := enum.(uint16)
@@ -8,6 +10,31 @@ func EnumToGo(enum interface{}) uint16 {
 
 // GoToEnum is golang value to mysql ENUM value
 func GoToEnum(enum interface{}) uint16 {
-	num := enum.(uint16)
-	return num + 1
+	switch num := enum.(type) {
+	case uint:
+		return uint16(num) + 1
+	case uint8:
+		return uint16(num) + 1
+	case uint16:
+		return uint16(num) + 1
+	case uint32:
+		return uint16(num) + 1
+	case uint64:
+		return uint16(num) + 1
+	case int:
+		return uint16(num) + 1
+	case int8:
+		return uint16(num) + 1
+	case int16:
+		return uint16(num) + 1
+	case int32:
+		return uint16(num) + 1
+	case int64:
+		return uint16(num) + 1
+	case interface{ EnumDescriptor() ([]byte, []int) }: //grpc
+		_, nums := num.EnumDescriptor()
+		return uint16(nums[0]) + 1
+	default:
+		panic(fmt.Sprintf("invalid type %T", enum))
+	}
 }
